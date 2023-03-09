b8d30f365d65414a24e0a552c65577bdf63ea64a
Tan Bunnchhay <bunchay1280@gmail.com>
2023-03-09 21:00:14 +0700

    new oop 05 and 06 (06 not done yet)

--- bin/rean_oop_04.dart
+++ bin/rean_oop_04.dart
@@ -1,3 +1,4 @@
+//#region CAR
 class Car{ // super class
   //#region FIELD
   int? _speed;
@@ -5,21 +6,31 @@ class Car{ // super class
   String? _color;
   //#endregion
 
-  double getSalePrice(){
-    return 0.0;
+  double? getSalePrice(){
+    return _regularPrice;
   }
 
+  Car(int speed, double price, String color){
+    _speed = speed;
+    _regularPrice = price;
+    _color = color;
+  }
 }
+//#endregion
 
+//#region TRUCK
 class Truck extends Car{
   int? _weight;
 
-  @override
-  double getSalePrice(){
-    return 0.0;
+  Truck(super.speed, super.price, super.color);
+
+  double? getSalePrice(){
+    return super.getSalePrice();
   }
 }
+//#endregion
 
+//#region FORD
 class Ford extends Car{
   //#region FIELD
   int? _year;
@@ -27,25 +38,55 @@ class Ford extends Car{
   //#endregion
 
   @override
-  double getSalePrice(){
-    return 0.0;
+  double? getSalePrice(){
+    return super.getSalePrice();
   }
-}
 
+  // constructor
+  Ford(int year, int manufacturerDiscount, int speed, double price, String color) : super(speed, price, color){
+    _year = year;
+    _manufacturerDiscount = manufacturerDiscount;
+  }
+
+}
+//#endregion
+
+//#region SEDAN
 class Sedan extends Car{
   int? _length;
 
   @override
-  double getSalePrice(){
-    return 0.0;
+  double? getSalePrice(){
+    return super.getSalePrice();
+  }
+
+  Sedan(int length, int speed, double price, String color) : super(speed, price, color){
+    _length = length;
   }
 }
+//#endregion
 
+//#region MY_OWN_AUTO_SHOP
 class MyOwnAutoShop{
   void main(){
+    // Create an instance of Sedan class and initialize all the fields
+    Sedan sedan = Sedan(12, 200, 5000, 'Red');
 
+    // Create two instances of the Ford class and initialize all the fields with appropriate
+    Ford ford2020 = Ford(2020, 10, 220, 10000, 'white');
+    Ford ford2022 = Ford(2022, 5,  250, 20000, 'black');
+
+    // Car class and initialize all the fields with appropriate values.
+    Car car = Car(50, 2000, 'Grean');
+
+    // Display the sale prices of all instance.
+    print('Price of Sedan: ${sedan.getSalePrice()}\$');
+    print('Price of Ford2020: ${ford2020.getSalePrice()}\$');
+    print('Price of Ford2022: ${ford2022.getSalePrice()}\$');
+    print('Price of Car: ${car.getSalePrice()}\$');
   }
 }
+//#endregion
 
 void main(){
   MyOwnAutoShop myOwnAutoShop = MyOwnAutoShop();
---
+++ bin/rean_oop_05.dart
@@ -0,0 +1,93 @@
+import 'package:meta/meta.dart' show protected;
+
+//#region CAKE
+abstract class Cake{
+  @protected
+  String? name;
+
+  @protected
+  double? rate;
+
+  Cake(this.name, this.rate);
+
+  double calcPrice();
+
+  @override
+  String toString(){
+    return '$name &\t$rate';
+  }
+}
+//#endregion
+
+//#region ORDER_CAKE
+class OrderCake extends Cake{
+  double? _weight;
+
+  @override
+  double calcPrice() {
+    return _weight! * super.rate!;
+  }
+
+  OrderCake(double weight, String name, double rate) : super(name, rate){
+    _weight = weight;
+  }
+}
+//#endregion
+
+//#region READY_MADE_CAKE
+class ReadyMadeCake extends Cake{
+  int? _quantity;
+
+  @override
+  double calcPrice() {
+    return super.rate! * _quantity!;
+  }
+
+  ReadyMadeCake(int quantity, String name, double rate) : super(name, rate){
+    _quantity = quantity;
+  }
+}
+//#endregion
+
+void main(){
+  print('<<<< -- Cake Application -- >>>>');
+
+  List<Cake> cakes = [];
+
+  for(int i = 0; i < 20; i++){
+    Cake cake = ReadyMadeCake(i+1, 'cake$i', i+2.5);
+    cake.name = 'cake$i';
+    cake.rate = i+2.5;
+    cakes.add(cake);
+  }
+
+  // c. display the total price for all types of cakes;
+  double totalPrice = 0.0;
+  double totalPriceReadyMadeCake = 0.0;
+  double quantity = 0;
+  double highestPrice = 0;
+  for(Cake cake in cakes){
+    totalPrice += cake.rate!;
+    totalPriceReadyMadeCake += cake.calcPrice();
+    quantity += cake.calcPrice()/cake.rate!;
+
+    // check the highest price
+    if(highestPrice < cake.rate!){
+      highestPrice = cake.rate!;
+    }
+  }
+
+  // display
+  print('total price for all types of cake: $totalPrice');
+  print('total price and the quantity sold for ready made cakes: $totalPriceReadyMadeCake\$ & ${quantity.toInt()} cakes');
+
+
+  // e. display the information for the cake that has been sold for the highest price.
+  for(Cake cake in cakes){
+    if(highestPrice == cake.rate!){
+      print('-----------------------');
+      print('The highest price of all Cake');
+      print('cake\'s name & cake\'s rate: ${cake.toString()}\$');
+    }
+  }
+}
